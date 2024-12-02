13f8624722b7a1b4e1a0d8bb79c81ead3250adfd
ProfSchrodinger <sabarimathavan24@gmail.com>
2024-12-02 15:21:55 -0500

    blank pages added

--- ridehail_flask/app.py
+++ ridehail_flask/app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return render_template('index.html')
+
+@app.route('/static-visualizations')
+def static_visualizations():
+    return render_template('static_visualizations.html')
+
+@app.route('/dynamic-visualizations')
+def dynamic_visualizations():
+    return render_template('dynamic_visualizations.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
