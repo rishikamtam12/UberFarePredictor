fa8e5e363c472f4840a1753706f1d70e6f82b012
ProfSchrodinger <sabarimathavan24@gmail.com>
2024-12-02 17:16:25 -0500

    added charts to flask

--- ridehail_flask/app.py
+++ ridehail_flask/app.py
@@ -34,9 +34,55 @@ def view_sample_data():
                            uber_table=uber_table, 
                            weather_table=weather_table)
 
-@app.route('/view-chart1')
-def view_chart1():
-    return render_template('chart1.html')
+@app.route('/comparision')
+def comparision():
+    # Load the comparison data CSV using the absolute path
+    comparision_data = pd.read_csv(os.path.join(STATIC_FOLDER, 'comparision_data.csv'))
+
+    # Convert the DataFrame to an HTML table
+    comparision_table = comparision_data.to_html(index=False, classes='table table-striped', border=0)
+
+    # Pass the table to the template
+    return render_template('comparision.html', comparision_table=comparision_table)
+
+@app.route('/view-chart/<chart_number>')
+def view_chart(chart_number):
+    # Define the image paths and descriptions for each pair of charts
+    chart_info = {
+        '1': {
+            'images': ['uber_fare_price_distribution.png', 'lyft_fare_price_distribution.png'],
+            'description': 'Visualization 1 - Fare price distribution (histogram and bar plot)'
+        },
+        '2': {
+            'images': ['uber_lyft_distance_price_distribution.png'],
+            'description': 'Visualization 2 - Distance vs Price'
+        },
+        '3': {
+            'images': ['uber_heatmap.png', 'lyft_heatmap.png'],
+            'description': 'Visualization 3 - Heatmaps [Weather metrics vs Price]'
+        },
+        '4': {
+            'images': ['chart4a.png', 'chart4b.png'],
+            'description': 'These charts offer a comparative analysis and visual representation of data distribution.'
+        }
+    }
+
+    # Get the chart data based on the button clicked
+    chart_data = chart_info.get(chart_number, None)
+
+    if not chart_data:
+        return 'Chart not found', 404
+
+    # Check if only 1 chart is available in the list
+    if len(chart_data['images']) == 1:
+        return render_template('view_chart.html', 
+                               chart_images=[chart_data['images'][0]], 
+                               chart_description=chart_data['description'])
+    else:
+        # Render the page for multiple charts
+        return render_template('view_chart.html', 
+                               chart_images=chart_data['images'], 
+                               chart_description=chart_data['description'])
 
 if __name__ == "__main__":
     app.run(debug=True)
